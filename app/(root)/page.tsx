99d102bac21bbe3df50a69863f45bc82143b52f2
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-01 00:13:48 +0530

    feat:added shadcn

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-primary-1 text-xl font-semibold text-seconday-3">
+      Anuvaad Sangam
+    </main>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,67 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors:{
+        "primary-1": '#E4DFFF',
+        'primary-2': '#C7C2FA',
+        'primary-3': '#9D97ED',
+        'primary-4': '#7165E3',
+        'primary-5': '#5141DC',
+        'primary-6': '#4130D9',
+        'primary-7': '#3223BE',
+        'primary-8': '#1B1367',
+        'primary-9': '#120D45',
+        'primary-10': '#090622',
+        'secondary-1':'#A4ECF4',
+        'secondary-2':'#1AC8DB',
+        'secondary-3':'#0F7580',
+        'secondary-4':'#042124',
+        'black': '#000000',
+        'white': '#FFFFFF',
+        'light-1': '#F9F9F9',
+        'light-2': '#F5F6FA',
+        'light-3': '#F3F3F3',
+        'light-4': '#EDEDED',
+        glassmorphism: "rgba(16, 16, 18, 0.60)",
+      },
+      boxShadow: {
+        "count-badge": "0px 0px 6px 2px rgba(219, 188, 159, 0.30)",
+        "groups-sidebar": "-30px 0px 60px 0px rgba(28, 28, 31, 0.50)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
