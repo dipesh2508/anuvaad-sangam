9edafdfe6f5836eb4bc999a55d4dace488579546
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-02 22:27:15 +0530

    chore:added logos

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="bg-primary-1 text-xl font-semibold text-seconday-3">
+      <Button>
       Anuvaad Sangam
+      </Button>
     </main>
   );
 }
