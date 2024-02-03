fff2ce51a5f7ddd9cd00ecae182d3dceed100944
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-02-03 22:16:31 +0530

    fix: changes in button

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-primary-1 text-xl font-semibold text-seconday-3">
-      <Button>
+      <Button variant='outline'>
       Anuvaad Sangam
       </Button>
     </main>
