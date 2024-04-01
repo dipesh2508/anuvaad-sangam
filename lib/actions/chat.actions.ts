1cc574d584bb63a96b11d45b30292b56bdb06002
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-04-01 20:05:18 +0530

    feat:Add sendMessage and fetchMessages functions

--- lib/actions/chat.actions.ts
+++ lib/actions/chat.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { Chat } from "../models/chat.model";
+import { Message } from "../models/message.model";
 import { User } from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
@@ -45,5 +46,50 @@ export async function findChat(curUserId: string, partnerUserId: string) {
     return existingChats._id;
   } catch (error: any) {
     throw new Error(`Failed to find chat.\nERROR:${error.message}`);
+  }
+}
+
+export async function sendMessage(
+  senderId: string,
+  conversationId: string,
+  body: string,
+) {
+  try {
+    connectToDB();
+
+    const message = await Message.create({
+      body1: body,
+      body2: body,
+      senderId,
+      language1: "en",
+      language2: "en",
+    });
+
+    const chat = await Chat.findById(conversationId);
+
+    chat.messages.push(message);
+
+    await chat.save();
+
+    return chat.messages; //Todo
+  } catch (error: any) {
+    throw new Error(`Failed to send message.\nERROR:${error.message}`);
+  }
+}
+
+export async function fetchMessages(conversationId: string) {
+  try {
+    connectToDB();
+
+    const chat = await Chat.findById(conversationId);
+
+    if (!chat) {
+      console.log(`No chat with conversation id ${conversationId} found`);
+      return null;
+    }
+
+    return chat.messages;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch messages.\nERROR:${error.message}`);
   }
 }
