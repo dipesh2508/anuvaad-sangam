b393dc961e4e72fd41d747815482180541f33248
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-04-02 22:21:05 +0530

    feat:Add Google Cloud Translate API and update translation actions

--- lib/actions/translation.actions.ts
+++
@@ -1,9 +0,0 @@
-import { translate, Translate } from "free-translate";
-
-export async function translateApi(text: string, translateTo: string) {
-    const translatedText = await translate(text, {
-        to: translateTo as Translate['to'],
-    });
-
-    return translatedText;
-}
