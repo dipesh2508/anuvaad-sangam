d71aee4dbc013f3f664abf17ce8a44e25aeee77a
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-04-02 13:08:45 +0530

    feat:added fetch message functionality

--- components/shared/ConversationBody.tsx
+++ components/shared/ConversationBody.tsx
@@ -1,18 +1,27 @@
-"use client";
 
 import { useRef } from "react";
 import MessageBox from "./MessageBox";
-const ConversationBody = ({conversationId}:{
+import { fetchMessages } from "@/lib/actions/chat.actions";
+const ConversationBody = async ({conversationId}:{
     conversationId: string;
 }) => {
 
-    const messages : any[] = [];
-    const bottomRef = useRef<HTMLDivElement>(null);
+    const messages = await fetchMessages(conversationId);
+
+    if(!messages){
+        return <div>no message</div>
+    }
+
+    const revMessages = messages.reverse()
+
+    // const bottomRef = useRef<HTMLDivElement>(null);
 
     return (
       <div className="flex-1 overflow-y-auto h-[70vh]">
-        <div ref={bottomRef} className="pt-24">
-          {messages.map((message, i) => (
+        <div 
+        // ref={bottomRef}
+         className="pt-24">
+          {revMessages.map((message, i) => (
             <MessageBox
               isLast={i === messages.length - 1}
               key={message.id}
--- components/shared/MessageBox.tsx
+++ components/shared/MessageBox.tsx
@@ -1,9 +1,25 @@
 import clsx from "clsx";
 import Image from "next/image";
 import { format } from "date-fns";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
 
-const MessageBox = ({ isLast, data }: { isLast: boolean; data: any }) => {
-  const isOwn = false;
+const MessageBox = async ({ isLast, data }: { isLast: boolean; data: any }) => {
+
+  const user = await currentUser();
+  if(!user){
+    redirect("/sign-in");
+  }
+
+  const userData = await fetchUser(user.id);
+
+  if (!userData) {
+    redirect("/onboarding");
+  }
+
+
+  const isOwn = true;
   
   const container = clsx("flex gap-3 p-4", isOwn && "justify-end");
   const avatar = clsx(isOwn && "order-2");
@@ -11,18 +27,18 @@ const MessageBox = ({ isLast, data }: { isLast: boolean; data: any }) => {
 
   const message = clsx(
     "text-sm w-fit overflow-hidden",
-    isOwn ? "bg-sky-500 text-white" : "bg-gray-200",
+    isOwn ? "bg-primary-4 text-white" : "bg-gray-200",
     data.image ? "rounded-md p-0" : "rounded-full py-2 px-3",
   );
 
   return (
     <div className={container}>
       <div className={avatar}>
-        <Image src={data.image} alt={data.name} />
+        <Image src={userData.image} width={36} height={36} className="rounded-full" alt={userData.name} />
       </div>
       <div className={body}>
         <div className="flex items-center gap-1">
-          <div className="text-sm text-gray-500">{data.sender.name}</div>
+          {/* <div className="text-sm text-gray-500">{data.sender.name}</div> */}
           <div className="text-xs text-gray-400">
             {format(new Date(data.createdAt), "p")}
           </div>
@@ -33,11 +49,11 @@ const MessageBox = ({ isLast, data }: { isLast: boolean; data: any }) => {
               alt="Image"
               height={288}
               width={288}
-              src={data.image}
+              src={userData.image}
               className="translate cursor-pointer object-cover transition hover:scale-110"
             />
           ) : (
-            <div>{data.body}</div>
+            <div>{data.body1}</div>
           )}
         </div>
         {isLast && (
