04a02752004363c3169b029aeae1527958134068
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-04-03 00:35:08 +0530

    Remove unused translation API route and add translation action

---
+++ lib/actions/translate.actions.ts
@@ -0,0 +1,14 @@
+"use server";
+import axios from "axios";
+
+export async function translation(message: string, language: string) {
+  try {
+    const response = await axios.post(
+      `http://localhost:8000/translate?text=${message}&target=${language}`,
+    );
+
+    if (response.data) return response.data;
+  } catch (error: any) {
+    throw new Error(`Unable to translate text.\nError:${error}`);
+  }
+}
