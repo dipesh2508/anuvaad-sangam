5aa1f30450ff636c85631f5777c7b23071ea0543
shariq740 <shariqmohd740@gmail.com>
2024-07-01 23:24:00 +0530

    chore:added semantics code

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,19 +1,20 @@
-import Hero from "@/components/about/hero"
-import Mv from "@/components/about/mv"
-import Team from "@/components/about/team"
-import PublicFooter from "@/components/shared/PublicFooter"
-import PublicNavBar from "@/components/shared/PublicNavBar"
+import Hero from "@/components/about/hero";
+import Mv from "@/components/about/mv";
+import Team from "@/components/about/team";
+import PublicFooter from "@/components/shared/PublicFooter";
+import PublicNavBar from "@/components/shared/PublicNavBar";
 
 const page = () => {
   return (
     <main className="text-seconday-3 relative overflow-hidden bg-light-4 text-xl font-semibold">
-        <PublicNavBar />
-        <Hero />
-        <Mv />
-        <Team />
-        <PublicFooter />
+      {/* added all the components of the About page   */}
+      <PublicNavBar />
+      <Hero />
+      <Mv />
+      <Team />
+      <PublicFooter />
     </main>
-  )
-}
+  );
+};
 
-export default page
+export default page;
